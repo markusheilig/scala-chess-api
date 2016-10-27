b54e0be03a84bccb7ca1c933aaa37e0480044f3f
Andreas Reinhardt <andreasreinhardt7@gmail.com>
2016-10-27 20:51:16 +0200

    Add ID to pieces and actions

--- src/main/scala/chess/api/Actions.scala
+++ src/main/scala/chess/api/Actions.scala
@@ -1,9 +1,11 @@
 package chess.api
 
+  
 trait Action {
+  val pieceId: Int
   val origin: (Int, Int)
   val target: (Int, Int)
 }
 
-case class Move(origin: (Int, Int), target: (Int, Int)) extends Action
-case class Castle(origin: (Int, Int), target: (Int, Int), targetKing: (Int, Int), targetRook: (Int, Int)) extends Action
+case class Move(pieceId: Int, origin: (Int, Int), target: (Int, Int)) extends Action
+case class Castle(pieceId: Int, origin: (Int, Int), target: (Int, Int), targetKing: (Int, Int), targetRook: (Int, Int)) extends Action
--- src/main/scala/chess/api/Pieces.scala
+++ src/main/scala/chess/api/Pieces.scala
@@ -6,11 +6,12 @@ object Color extends Enumeration {
 
 trait Piece {
   val color: Color.Value
+  val id: Int
 }
 
-case class King(color: Color.Value) extends Piece
-case class Queen(color: Color.Value) extends Piece
-case class Rook(color: Color.Value) extends Piece
-case class Bishop(color: Color.Value) extends Piece
-case class Knight(color: Color.Value) extends Piece
-case class Pawn(color: Color.Value) extends Piece
+case class King(color: Color.Value, id: Int) extends Piece
+case class Queen(color: Color.Value, id: Int) extends Piece
+case class Rook(color: Color.Value, id: Int) extends Piece
+case class Bishop(color: Color.Value, id: Int) extends Piece
+case class Knight(color: Color.Value, id: Int) extends Piece
+case class Pawn(color: Color.Value, id: Int) extends Piece
