dad6b14d624396b64b3064ac50ca9fbabb4940f9
Andreas Reinhardt <andreasreinhardt7@gmail.com>
2016-10-30 11:28:09 +0100

    Add choice trait and possible choices of pawn when reaching last file

--- src/main/scala/chess/api/Actions.scala
+++ src/main/scala/chess/api/Actions.scala
@@ -1,11 +1,31 @@
 package chess.api
 
-  
-trait Action {
+import chess.api.MoveAndChangeChoice.MoveAndChangeChoice
+
+
+sealed trait Action {
   val pieceId: Int
   val origin: (Int, Int)
   val target: (Int, Int)
 }
 
 case class Move(pieceId: Int, origin: (Int, Int), target: (Int, Int)) extends Action
+
+case class MoveAndChange(pieceId: Int, origin: (Int, Int), target: (Int, Int), choice: MoveAndChangeChoice) extends Action with Choice
+
 case class Castle(pieceId: Int, origin: (Int, Int), target: (Int, Int), targetKing: (Int, Int), targetRook: (Int, Int)) extends Action
+
+trait Choice {
+  val choice: ChoiceVal
+}
+
+trait ChoiceVal
+
+object MoveAndChangeChoice{
+  trait MoveAndChangeChoice extends ChoiceVal
+
+  case object Queen extends MoveAndChangeChoice
+  case object Rook extends MoveAndChangeChoice
+  case object Knight extends MoveAndChangeChoice
+  case object Bishop extends MoveAndChangeChoice
+}
