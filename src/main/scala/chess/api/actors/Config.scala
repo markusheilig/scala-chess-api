26043a0bc14727e02a315276c3ee711bf7ccf8db
Markus Heilig <markusheilig@gmx.net>
2016-11-06 13:07:03 +0100

    parse actor config from github

--- src/main/scala/chess/api/actors/Config.scala
+++ src/main/scala/chess/api/actors/Config.scala
@@ -1,17 +1,24 @@
 package chess.api.actors
 
+import java.net.URL
+
 import akka.actor.ActorRef
+import com.typesafe.config.ConfigFactory
 
 case class RegisterObserver(observer: ActorRef)
 case class UnregisterObserver(observer: ActorRef)
 
 object Config {
-  val actorSystem = "chess"
-  val actorName = "controller"
-  val protocol = "tcp"
-  val hostname = "127.0.0.1"
-  val port = 2552
-  val controllerActorPath = s"akka.$protocol://$actorSystem@$hostname:$port/user/$actorName"
+  def controllerActorPath = {
+    val url = new URL("https://raw.githubusercontent.com/markusheilig/scala-chess/master/src/main/resources/application.conf")
+    val config = ConfigFactory.parseURL(url)
+    val actorSystem = config.getString("akka.chess.systemName")
+    val actorName = config.getString("akka.chess.actorName")
+    val hostname = config.getString("akka.remote.netty.typ.hostname")
+    val port = config.getString("akka.remote.netty.tcp.port")
+    val protocol = "tcp"
+    s"akka.$protocol://$actorSystem@$hostname:$port/user/$actorName"
+  }
 }
 
 
