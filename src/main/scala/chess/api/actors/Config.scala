bde728b4a311c05c6cc40d4222662ffa17b5096c
Markus Heilig <markusheilig@gmx.net>
2016-11-06 11:35:33 +0100

    update config

--- src/main/scala/chess/api/actors/Config.scala
+++ src/main/scala/chess/api/actors/Config.scala
@@ -6,7 +6,12 @@ case class RegisterObserver(observer: ActorRef)
 case class UnregisterObserver(observer: ActorRef)
 
 object Config {
-  val controllerActorPath = "akka.tcp://chess@127.0.0.1:2552/user/controller"
+  val actorSystem = "chess"
+  val actorName = "controller"
+  val protocol = "tcp"
+  val hostname = "127.0.0.1"
+  val port = 2552
+  val controllerActorPath = s"akka.$protocol://$actorSystem@$hostname:$port/user/$actorName"
 }
 
 
