48caa99d03312c4c1f0c9378a3330c1813e6b7b0
Markus Heilig <markusheilig@gmx.net>
2016-11-06 19:20:31 +0100

    replace Map with Iterable

--- src/main/scala/chess/api/ChessBoard.scala
+++ src/main/scala/chess/api/ChessBoard.scala
@@ -1,4 +1,4 @@
 package chess.api
 
 case class Update(chessBoard: ChessBoard)
-case class ChessBoard(dimX: Int, dimY: Int, pieces: Map[(Int, Int), Piece])
+case class ChessBoard(dimX: Int, dimY: Int, pieces: Iterable[((Int, Int), Piece)])
