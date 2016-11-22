d19d431542c64fb888d8b7defa26f6af6ffc9e0c
Andreas Reinhardt <reinhardt@bond-it.de>
2016-11-22 10:55:31 +0100

    List to Seq

--- src/main/scala/chess/api/Actions.scala
+++ src/main/scala/chess/api/Actions.scala
@@ -3,14 +3,18 @@ package chess.api
 import chess.api.MoveAndChangeChoice.MoveAndChangeChoice
 
 trait Atomic {
-  val actions = List()
+  val actions = Seq()
 }
 
-sealed trait Action extends Iterable[Action] {
+sealed trait Action extends Seq[Action] {
   val pieceId: Int
   val origin: Option[Position]
   val target: Position
-  val actions: List[Action]
+  val actions: Seq[Action]
+
+  override def length: Int = actions.length
+
+  override def apply(idx: Int): Action = actions.apply(idx)
 
   override def iterator: Iterator[Action] = actions.iterator
 }
@@ -25,11 +29,11 @@ case class PutInitial(target: Position, piece: Piece, origin: Option[Position] =
   override val pieceId: Int = piece.id
 }
 
-case class Move(pieceId: Int, target: Position, actions: List[Action], origin: Option[Position] = None) extends Action
+case class Move(pieceId: Int, target: Position, actions: Seq[Action], origin: Option[Position] = None) extends Action
 
-case class Castle(pieceId: Int, target: Position, actions: List[Action], origin: Option[Position] = None) extends Action
+case class Castle(pieceId: Int, target: Position, actions: Seq[Action], origin: Option[Position] = None) extends Action
 
-case class MoveAndChange(pieceId: Int, target: Position, actions: List[Action], choice: MoveAndChangeChoice, origin: Option[Position] = None) extends Action with Choice
+case class MoveAndChange(pieceId: Int, target: Position, actions: Seq[Action], choice: MoveAndChangeChoice, origin: Option[Position] = None) extends Action with Choice
 
 trait Choice {
   val choice: ChoiceVal
