bc802aa62a156801aa59e30fed22534f9542a241
Andreas Reinhardt <reinhardt@bond-it.de>
2016-11-22 14:35:05 +0100

    Make Actions great again!

--- src/main/scala/chess/api/Actions.scala
+++ src/main/scala/chess/api/Actions.scala
@@ -1,16 +1,17 @@
 package chess.api
 
-import chess.api.MoveAndChangeChoice.MoveAndChangeChoice
+import chess.api.MoveAndReplaceChoice.MoveAndChangeChoice
 
 trait Atomic {
   val actions = Seq()
 }
 
 sealed trait Action extends Seq[Action] {
-  val pieceId: Int
-  val origin: Option[Position]
-  val target: Position
-  val actions: Seq[Action]
+  def pieceId: Int
+
+  def target: Position
+
+  def actions: Seq[Action]
 
   override def length: Int = actions.length
 
@@ -21,19 +22,40 @@ sealed trait Action extends Seq[Action] {
 
 case class InvalidAction(error: String, action: Action)
 
-case class Remove(pieceId: Int, target: Position, origin: Option[Position] = None) extends Action with Atomic
+case class Remove(pieceId: Int, target: Position) extends Action with Atomic
 
-case class Put(pieceId: Int, target: Position, origin: Option[Position] = None) extends Action with Atomic
+case class Put(pieceId: Int, target: Position) extends Action with Atomic
 
-case class PutInitial(target: Position, piece: Piece, origin: Option[Position] = None) extends Action with Atomic {
+case class PutInitial(target: Position, piece: Piece) extends Action with Atomic {
   override val pieceId: Int = piece.id
 }
 
-case class Move(pieceId: Int, target: Position, actions: Seq[Action], origin: Option[Position] = None) extends Action
+case class Move(pieceId: Int, remove: Remove, put: Put, removeAtTarget: Option[Remove]) extends Action {
+  override def target: Position = put.target
 
-case class Castle(pieceId: Int, target: Position, actions: Seq[Action], origin: Option[Position] = None) extends Action
+  override def actions: Seq[Action] = Seq(Some(remove), removeAtTarget, Some(put)) collect { case Some(a) => a }
+}
 
-case class MoveAndChange(pieceId: Int, target: Position, actions: Seq[Action], choice: MoveAndChangeChoice, origin: Option[Position] = None) extends Action with Choice
+case class Castle(pieceId: Int, kingMove: Move, rookMove: Move) extends Action {
+
+  override def target: Position = kingMove.remove.target
+
+  override def actions: Seq[Action] = Seq(kingMove, rookMove)
+
+}
+
+case class Replace(pieceId: Int, remove: Remove, putInitial: PutInitial) extends Action {
+  override def target: Position = remove.target
+
+  override def actions: Seq[Action] = Seq(remove, putInitial)
+}
+
+
+case class MoveAndReplace(pieceId: Int, move: Move, replace: Replace, choice: MoveAndChangeChoice) extends Action with Choice {
+  override def target: Position = move.target
+
+  override def actions: Seq[Action] = Seq(move, replace)
+}
 
 trait Choice {
   val choice: ChoiceVal
@@ -41,11 +63,16 @@ trait Choice {
 
 trait ChoiceVal
 
-object MoveAndChangeChoice{
+object MoveAndReplaceChoice {
+
   trait MoveAndChangeChoice extends ChoiceVal
 
   case object Queen extends MoveAndChangeChoice
+
   case object Rook extends MoveAndChangeChoice
+
   case object Knight extends MoveAndChangeChoice
+
   case object Bishop extends MoveAndChangeChoice
+
 }
