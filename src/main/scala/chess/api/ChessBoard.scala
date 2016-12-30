d47c2fdca5ce5a3dd4c5d7be34beb69a29093fbf
Markus Heilig <markusheilig@gmx.net>
2016-12-30 12:19:40 +0100

    add winner

--- src/main/scala/chess/api/ChessBoard.scala
+++ src/main/scala/chess/api/ChessBoard.scala
@@ -1,5 +1,5 @@
 package chess.api
 
-case class Update(chessBoard: ChessBoard)
+case class Update(chessBoard: ChessBoard, winner: Option[Color.Value] = None)
 case class QueryValidActions(position: Position)
 case class ChessBoard(dimX: Int, dimY: Int, pieces: Seq[(Position, Piece)])
